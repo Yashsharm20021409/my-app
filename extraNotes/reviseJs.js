29891ee100d9310eb58cd69a5740edd8ff749c8c
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2023-01-25 23:23:32 +0530

    update

---
+++ extraNotes/reviseJs.js
@@ -0,0 +1,17 @@
+// let :- eslye aya ki agr hm 2 jga same varibale use kr rhe h to error mile which is not getting if we use var
+// const :- ekbr declare krne k bd value change ni ho skti varibale ki
+
+// object :-
+let ob1 = {
+  m: 32,
+  name: "Yash",
+  func: function name() {
+    console.log("ajufh");
+  },
+};
+console.log(ob1, ob1.m, ob1.name, ob1.func); //access like that
+
+
+//Read about:-(also explain in video 3) diff btw function and arrow function
+// callbacks
+// promises
